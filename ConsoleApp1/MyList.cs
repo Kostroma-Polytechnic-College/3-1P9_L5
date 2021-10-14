a70892ef66d3e0e2d46e560304701196199e7a4b
SwiftKillers <yuri.silenok@gmail.com>
2021-10-14 09:07:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyList<UserType>
+    {
+        public class Item
+        {
+            public UserType Value;
+            public Item Next;
+        }
+        Item head;
+
+        public void Add(UserType value)
+        {
+            Item newItem = new Item() { Value = value };
+            if(head == null)
+            {
+                head = newItem;
+                return;
+            }
+            Item currentItem = head;
+            while (currentItem.Next != null)
+                currentItem = currentItem.Next;
+            currentItem.Next = newItem;
+        }
+
+        public UserType Get(int index)
+        {
+            Item currentItem = head;
+            for (int i = 0; i < index; i++)
+                currentItem = currentItem.Next;
+            return currentItem.Value;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int[] m = new int[] { x, y };
+            //Console.WriteLine(x);
+            Test(ref m);
+            foreach (var item in m)
+            {
+                Console.WriteLine(item);
+            }
+            //Console.WriteLine(x);
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("2");
+            myList.Add("3");
+            Console.WriteLine(myList.Get(1));
+
+            Console.ReadKey();
+        }
+        static void Test(ref int[] m)
+        {
+            m = new int[] { m[0], m[1] };
+            for (int i = 0; i < m.Length; i++)
+            {
+                m[i]++;
+            }
+        }
+        static void Test(int x)
+        {
+            x++;
+        }
+    }
+}
